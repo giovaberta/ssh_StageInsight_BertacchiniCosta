dcc4b63ac94f2bcc37437337f30f317c6916b24d
giovaberta <giovanni.bertacchini@fermi.mo.it>
2026-04-11 11:45:50 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -9,8 +9,47 @@ async def make_up():
     await user.insert_one(ut_user)
     await user.insert_one(ut_guest)
 
-    #Request
-    keys = ['dati_ets', 'dati_alunno', 'percorso_studente', 'prima_esperienza', 'fonte_info', 'motivazioni', 'ruolo_stage', 'rispetto_orari', 'miglioramento_incarichi', 'lavoro_gruppo', 'empatia', 'accettazione_punti_vista', 'comunicazione_efficace', 'gestione_imprevisti', 'chiedere_aiuto', 'curiosita_motivazione', 'utilita_competenze', 'altre_cose_imparate', 'crescita_comunicazione', 'imparato_stage', 'contesto_competenze', 'aspetti_volontariato', 'felicita_classe', 'regole_scuola', 'parte_gruppo', 'soddisfazione_voti', 'relazioni_classe', 'conoscenza_bisogni', 'supporto_famiglia']
-    for key in keys:
-        await form.insert_one({key:{}})
+
+    await form.insert_many([
+        {"dati_ets":{"type":"testo_breve","risp":{}}},
+        {"dati_alunno":{"type":"testo_breve","risp":{}}},
+        {"percorso_studente":{"type":"testo_breve","risp":{}}},
+        {"prima_esperienza":{"type":"si_no","risp":{}}},
+        {"fonte_info":{"type":"menu_tendina","risp":{},"possible_risp":["Progetto Cantieri Giovani","Dalla scuola","Dall'informagiovani","Dai centri giovani","Dal CSV Terre Estensi","Materiale promozionale","Servizi sociali","Dalla mia famiglia","Da un amico"]}},
+        {"motivazioni":{"type":"scelta_multipla","risp":{},"possible_risp":["Il confronto con i miei insegnanti","Gli stimoli degli amici","Gli stimoli della mia famiglia","La curiosità di provare qualcosa di nuovo","Le mie convinzioni personali","Qualche cosa che mi è successo"]}}
+    ])
+
+    await form.insert_many([
+        {"ruolo_stage":{"type":"testo_breve","risp":{}}},
+        {"rispetto_orari":{"type":"scala_valutazione","risp":{}}},
+        {"miglioramento_incarichi":{"type":"scala_valutazione","risp":{}}},
+        {"lavoro_gruppo":{"type":"scala_valutazione","risp":{}}},
+        {"empatia":{"type":"scala_valutazione","risp":{}}},
+        {"accettazione_punti_vista":{"type":"scala_valutazione","risp":{}}}
+    ])
+
+    await form.insert_many([
+        {"comunicazione_efficace":{"type":"scala_valutazione","risp":{}}},
+        {"chiedere_aiuto":{"type":"scala_valutazione","risp":{}}},
+        {"curiosita_motivazione":{"type":"scala_valutazione","risp":{}}},
+        {"utilita_competenze":{"type":"scala_valutazione","risp":{}}},
+        {"altre_cose_imparate":{"type":"testo_lungo","risp":{}}},
+        {"crescita_comunicazione":{"type":"scala_valutazione","risp":{}}}
+    ])
+
+    await form.insert_many([
+        {"imparato_stage":{"type":"scala_valutazione","risp":{},"possible_risp":["Problem solving","Empatia","Adattabilità","Autocontrollo","Lavoro di squadra/networking","Sicurezza in sé stessi","Spirito di collaborazione","Volontà di apprendere","Creatività e pensiero critico"]}},
+        {"contesto_competenze": {"type": "scala_valutazione", "risp": {}, "possible_risp":["Nel mondo della scuola","Nel mondo del lavoro","Nell'attività di volontariato","Nel mio contesto di amici","In famiglia"]}},
+        {"aspetti_volontariato": {"type": "scala_valutazione", "risp": {}, "possible_risp":["Conoscere nuove persone/realtà al di fuori della scuola","Impegnarsi in attività manuali","Scoprire nuovi interessi","Scoprire nuove capacità","Svolgere attività di tuo interesse","Lavorare in gruppo"]}},
+        {"felicita_classe": {"type": "scala_valutazione", "risp": {}}},
+        {"regole_scuola": {"type": "scala_valutazione", "risp": {}}},
+        {"parte_gruppo": {"type": "scala_valutazione", "risp": {}}}
+    ])
+
+    await form.insert_many([
+        {"soddisfazione_voti": {"type": "scala_valutazione", "risp": {}}},
+        {"relazioni_classe": {"type": "scala_valutazione", "risp": {}}},
+        {"conoscenza_bisogni": {"type": "scala_valutazione", "risp": {}}},
+        {"supporto_famiglia": {"type": "scala_valutazione", "risp": {}}}
+    ])
 asyncio.run(make_up())
